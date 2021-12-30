c4645dc0a4174b75a409ec4321afc06604cbd33f
liangjianwei <1325649060@qq.com>
2021-12-30 16:38:47 +0800

    feat(springboot自动配置探究): springboot自动配置探究例子

---
+++ springboot-work/src/main/java/com/ljw/springbootwork/SpringbootWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ljw.springbootwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWorkApplication.class, args);
+    }
+
+}
---
+++ springboot-work/src/test/java/com/ljw/springbootwork/SpringbootWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.springbootwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
