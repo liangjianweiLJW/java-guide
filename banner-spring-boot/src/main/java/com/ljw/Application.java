17dbbbcb68887b43cd1cf769ff44d19c1f32b9f9
liangjianwei <1325649060@qq.com>
2022-01-10 15:25:19 +0800

    feat(添加模块banner-spring-boot): banner使用例子

---
+++ banner-spring-boot/src/main/java/com/ljw/Application.java
@@ -0,0 +1,16 @@
+package com.ljw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: todo
+ * @Author: jianweil
+ * @date: 2022/1/10 9:29
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
